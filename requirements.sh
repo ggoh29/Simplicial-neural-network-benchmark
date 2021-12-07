e113c8d71e569c516ce2d3aacb3ede624e4a6bde
ggoh29 <ggoh29@yahoo.com.sg>
2021-12-08 00:13:39 +0800

    updated requirments

---
+++ requirements.sh
@@ -0,0 +1,10 @@
+pip install numpy
+pip install scikit-image
+pip install scipy
+
+pip install torch==1.8.1
+pip install torch-scatter==2.0.8 -f https://pytorch-geometric.com/whl/torch-1.8.0+cu111.html
+pip install torchvision==0.9.1 -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch-sparse==0.6.12 -f https://pytorch-geometric.com/whl/torch-1.8.0+cu111.html
+pip install torch-cluster==1.5.9 -f https://pytorch-geometric.com/whl/torch-1.8.0+cu111.html
+pip install torch-geometric
