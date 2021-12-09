af94098b04163fee25ce7d18812fdf59dd82ba1a
ggoh29 <ggoh29@yahoo.com.sg>
2021-12-09 09:14:35 +0800

    Added implementation of GAT

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from dataset_processor.SuperpixelLoader import SimplicialComplexDataset
 from dataset_processor.EdgeFlow import PixelBasedEdgeFlow, RandomBasedEdgeFlow, RAGBasedEdgeFlow
 from torch.utils.data import DataLoader
-from models.GNN.model import GCN, GAT
+from models.GNN.model import GCN, GAT, GAT1
 from models.GNN.GNNProcessor import GNNProcessor
 from models.SNN_Ebli.model_E import SNN_Ebli
 from models.SNN_Ebli.SNNEbliProcessor import SNNEbliProcessor
@@ -15,32 +15,30 @@ import numpy as np
 from datetime import timedelta
 
 batch_size = 8
-superpixel_size = 50
+superpixel_size = 25
 dataset = datasets.MNIST
 # dataset = datasets.CIFAR10
 edgeFlow = PixelBasedEdgeFlow
 # edgeFlow = RAGBasedEdgeFlow
 # edgeFlow = RandomBasedEdgeFlow
 
-print(DEVICE)
-
 processor_type = GNNProcessor()
 # processor_type = SNNEbliProcessor()
 # processor_type = SNNBunchProcessor()
-output_size = 4
+output_size = 2
 output_suffix = 0
 if __name__ == "__main__":
 
     # GNN = SNN_Bunch(5, 10, 15, output_size).to(DEVICE)
     # GNN = SNN_Ebli(5, 10, 15, output_size).to(DEVICE)
     # GNN = GCN(5, output_size).to(DEVICE)
-    GNN = GAT(5, output_size).to(DEVICE)
+    GNN = GAT1(5, output_size).to(DEVICE)
     model_parameters = filter(lambda p: p.requires_grad, GNN.parameters())
     params = sum([np.prod(p.size()) for p in model_parameters])
-    # print(params)
+    print(params)
 
     train_data = SimplicialComplexDataset('./data', dataset, superpixel_size, edgeFlow, processor_type, train=True)
-    train_dataset = DataLoader(train_data, batch_size=batch_size, collate_fn=processor_type.batch, num_workers=4, shuffle=True)
+    train_dataset = DataLoader(train_data, batch_size=batch_size, collate_fn=processor_type.batch, num_workers=4, shuffle=True, pin_memory=True)
     write_file_name = f"./results/{train_data.get_name()}_{output_suffix}"
 
     optimizer = torch.optim.Adam(GNN.parameters(), lr=0.001, weight_decay=5e-4)
@@ -51,7 +49,7 @@ if __name__ == "__main__":
     del train_dataset
 
     test_data = SimplicialComplexDataset('./data', dataset, superpixel_size, edgeFlow, processor_type, train=False)
-    test_dataset = DataLoader(test_data, batch_size=batch_size, collate_fn=processor_type.batch, num_workers=4, shuffle=True)
+    test_dataset = DataLoader(test_data, batch_size=batch_size, collate_fn=processor_type.batch, num_workers=4, shuffle=True, pin_memory=True)
 
     _, test_acc = test(GNN, test_dataset, processor_type)
 
--- models/GNN/model.py
+++ models/GNN/model.py
@@ -4,6 +4,8 @@ from torch_geometric.nn import GCNConv, GATConv
 from torch_geometric.nn import global_mean_pool
 import torch.nn.functional as F
 from models.nn_utils import chebyshev, unpack_feature_dct_to_L_X_B, convert_indices_and_values_to_sparse
+from constants import DEVICE
+import numpy as np
 
 
 class GCN(nn.Module):
@@ -14,10 +16,7 @@ class GCN(nn.Module):
         self.conv1 = GCNConv(input_size, f_size)
         self.conv2 = GCNConv(f_size, f_size)
         self.conv3 = GCNConv(f_size, f_size)
-        self.layer1 = nn.Linear(f_size, output_size)
-        self.layer2 = nn.Linear(f_size, output_size)
-        self.layer3 = nn.Linear(f_size, output_size)
-        self.layer_final = nn.Linear(3 * output_size, output_size)
+        self.layer_final = nn.Linear(3 * f_size, output_size)
 
     def forward(self, feature_dct):
         L, X, batch = unpack_feature_dct_to_L_X_B(feature_dct)
@@ -31,11 +30,8 @@ class GCN(nn.Module):
         x2 = F.relu(self.conv2(x1, adjacency))
         x3 = F.relu(self.conv3(x2, adjacency))
 
-        x1 = self.layer1(global_mean_pool(x1, batch[0]))
-        x2 = self.layer2(global_mean_pool(x2, batch[0]))
-        x3 = self.layer3(global_mean_pool(x3, batch[0]))
-
-        x = torch.cat([x1, x2, x3], dim = 1)
+        x = torch.cat([x1, x2 ,x3], dim = 1)
+        x = global_mean_pool(x, batch[0])
 
         return F.softmax(self.layer_final(x), dim = 1)
 
@@ -49,7 +45,7 @@ class GAT(nn.Module):
         self.conv1 = GATConv(input_size, f_size)
         self.conv2 = GATConv(f_size, f_size)
         self.conv3 = GATConv(f_size, f_size)
-        self.layer = nn.Linear(f_size, output_size)
+        self.layer = nn.Linear(3 * f_size, output_size)
 
     def forward(self, features_dct):
         L, X, batch = unpack_feature_dct_to_L_X_B(features_dct)
@@ -62,5 +58,56 @@ class GAT(nn.Module):
         x2 = F.relu(self.conv2(x1, adjacency))
         x3 = F.relu(self.conv3(x2, adjacency))
 
-        x = global_mean_pool((x1 + x2 + x3)/3, batch[0])
+        x = torch.cat([x1, x2 ,x3], dim = 1)
+        x = global_mean_pool(x, batch[0])
+
         return F.softmax(self.layer(x), dim = 1)
+
+
+class GATLayer(nn.Module):
+
+    def __init__(self, input_size, output_size):
+        super().__init__()
+        self.a = nn.Linear(2 * output_size, 1)
+        self.layer = nn.Linear(input_size, output_size)
+
+    def forward(self, features, edgelist):
+        features = self.layer(features)
+        n = torch.max(edgelist).item() + 1
+
+        src = features.unsqueeze(0).expand(n, -1, -1)
+        tgt = features.unsqueeze(1).expand(-1, n, -1)
+        h = torch.cat([src,tgt],dim=2)
+
+        attention = self.a(h).squeeze(2)
+        # attention = F.normalize(attention, dim = 1)
+        attention = F.softmin(attention, dim = 1)
+        print(attention)
+        return torch.matmul(attention, features)
+
+
+class GAT1(nn.Module):
+    def __init__(self, input_size, output_size):
+        super().__init__()
+
+        f_size = 6
+        self.gat1 = GATLayer(input_size, f_size)
+        self.gat2 = GATLayer(f_size, f_size)
+        self.gat3 = GATLayer(f_size, f_size)
+        self.layer = nn.Linear(3 * f_size, output_size)
+
+    def forward(self, features_dct):
+        L, X, batch = unpack_feature_dct_to_L_X_B(features_dct)
+
+        adjacency = L[0].coalesce().indices()
+        # weights = torch.abs(L[0].coalesce().values())
+        features = X[0]
+
+        x1 = F.relu(self.gat1(features, adjacency))
+        x2 = F.relu(self.gat2(x1, adjacency))
+        x3 = F.relu(self.gat3(x2, adjacency))
+
+        x = torch.cat([x1, x2, x3], dim=1)
+        x = global_mean_pool(x, batch[0])
+
+        return F.softmax(self.layer(x), dim=1)
