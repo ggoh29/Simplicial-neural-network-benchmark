d702c2b7af401ea6391431eb8c4a7b58ab5f1d0f
ggoh29 <56758342+ggoh29@users.noreply.github.com>
2021-12-10 06:37:49 +0000

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
     NN_list = [gnn, Ebli_nn, Bunch_nn, sat_nn]
     for output_suffix in range(10):
         for processor_type, NN in NN_list:
-            run(processor_type, NN, output_suffix)
+            run(processor_type, NN.to(DEVICE), output_suffix)
 
 
 
