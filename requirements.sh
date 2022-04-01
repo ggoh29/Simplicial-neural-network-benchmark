25282e6c15e5dd9843990e28e5161a27502855c6
ggoh29 <ggoh29@yahoo.com.sg>
2022-04-01 23:06:47 +0800

    Changed requirements

--- requirements.sh
+++ requirements.sh
@@ -3,7 +3,7 @@ pip install scikit-image
 pip install scipy
 
 pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 -f https://download.pytorch.org/whl/torch_stable.html
-  torch-sparse==latest+cu111 \
+pip install torch-sparse==latest+cu111 \
   torch-scatter==latest+cu111 \
   torch-cluster==latest+cu111 \
   -f https://pytorch-geometric.com/whl/torch-1.8.0+cu111.html
